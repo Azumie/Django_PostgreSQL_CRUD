01d07f5417369b32c4248f61d6e7b2e2d1819e00
Nella <azumie250@gmail.com>
2023-03-26 21:21:28 -0400

    Creando tareas

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-
+from .models import Task
 # Create your views here.
 # va a intentar renderizar un archivo, pero cuando ocurra algo. Para ello creamos un archivo llamado url
 def list_tasks(request):
     return render(request, 'list_tasks.html')
 
 def create_task(request):
-    print(request.POST)
+    task = Task(title=request.POST['title'], description=request.POST['description'])
+    task.save()
+    print(request.POST) 
     return redirect('/tasks/')
