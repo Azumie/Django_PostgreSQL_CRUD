25f22fd2ed38ea1f14f7b102c4d53b15b89bf57e
Nella <azumie250@gmail.com>
2023-03-26 21:57:17 -0400

    Listando tareas

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,8 @@ from .models import Task
 # Create your views here.
 # va a intentar renderizar un archivo, pero cuando ocurra algo. Para ello creamos un archivo llamado url
 def list_tasks(request):
-    return render(request, 'list_tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'list_tasks.html', {'tasks': tasks})
 
 def create_task(request):
     task = Task(title=request.POST['title'], description=request.POST['description'])
